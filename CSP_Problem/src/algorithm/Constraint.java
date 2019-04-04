4a6eeaae53248dccaf02386cd2769dbb4ce00036
Ola Druciak <ola.druciak@wp.pl>
2019-04-04 14:03:47 +0200

    Added class diagram

--- CSP_Problem/src/algorithm/Constraint.java
+++ CSP_Problem/src/algorithm/Constraint.java
@@ -5,5 +5,5 @@ import java.util.HashMap;
 
 public interface Constraint
 {
-    public boolean canBeHereByConstraint(int value, int row, int col, HashMap<Integer, ArrayList<Integer>> matrix);
+    boolean canBeHereByConstraint(int value, int row, int col, HashMap<Integer, ArrayList<Integer>> matrix);
 }
